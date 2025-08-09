ff3e6608b9e3848c10e7592056849447b2ccc9b6
junwang <wang.jun16@163.com>
2025-08-10 00:53:46 +0900

    add android and ios module

--- packages/app/lib/main.dart
+++ packages/app/lib/main.dart
@@ -8,6 +8,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main(){
-
-  runApp(const ProviderScope(child: PoyopoyoApp()));
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(PoyopoyoApp());
 }
