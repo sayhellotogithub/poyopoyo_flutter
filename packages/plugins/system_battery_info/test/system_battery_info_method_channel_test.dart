b5a1b68de89aa148d231a12636113faf49c2b824
junwang <wang.jun16@163.com>
2025-10-06 22:34:08 +0900

    add battery_info test

--- packages/plugins/system_battery_info/test/system_battery_info_method_channel_test.dart
+++ packages/plugins/system_battery_info/test/system_battery_info_method_channel_test.dart
@@ -1,27 +1,28 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:system_battery_info/system_battery_info_method_channel.dart';
+import 'package:system_battery_info/system_battery_info.dart';
 
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
 
-  MethodChannelSystemBatteryInfo platform = MethodChannelSystemBatteryInfo();
-  const MethodChannel channel = MethodChannel('system_battery_info');
+  const channel = MethodChannel('system_battery_info');
+  TestWidgetsFlutterBinding.ensureInitialized();
 
   setUp(() {
-    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger.setMockMethodCallHandler(
-      channel,
-      (MethodCall methodCall) async {
-        return '42';
-      },
-    );
+    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
+        .setMockMethodCallHandler(channel, (MethodCall call) async {
+          if (call.method == 'getBatteryLevel') return 42;
+          return null;
+        });
   });
 
   tearDown(() {
-    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger.setMockMethodCallHandler(channel, null);
+    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
+        .setMockMethodCallHandler(channel, null);
   });
 
-  test('getPlatformVersion', () async {
-    expect(await platform.getPlatformVersion(), '42');
+  test('mocked battery level', () async {
+    final result = await SystemBatteryInfo().getBatteryLevel();
+    expect(result, 42);
   });
 }
--- packages/plugins/system_battery_info/test/system_battery_info_test.dart
+++ packages/plugins/system_battery_info/test/system_battery_info_test.dart
@@ -1,29 +1,37 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:system_battery_info/system_battery_info.dart';
-import 'package:system_battery_info/system_battery_info_platform_interface.dart';
-import 'package:system_battery_info/system_battery_info_method_channel.dart';
 import 'package:plugin_platform_interface/plugin_platform_interface.dart';
+import 'package:system_battery_info/system_battery_info.dart';
+import 'package:system_battery_info/system_battery_info_method_channel.dart';
+import 'package:system_battery_info/system_battery_info_platform_interface.dart';
 
 class MockSystemBatteryInfoPlatform
     with MockPlatformInterfaceMixin
     implements SystemBatteryInfoPlatform {
+  @override
+  Future<int?> getBatteryLevel() {
+    return Future.value(42);
+  }
 
   @override
-  Future<String?> getPlatformVersion() => Future.value('42');
+  Future<String?> getBatteryState() {
+    return Future.value('charging');
+  }
 }
 
 void main() {
-  final SystemBatteryInfoPlatform initialPlatform = SystemBatteryInfoPlatform.instance;
+  final SystemBatteryInfoPlatform initialPlatform =
+      SystemBatteryInfoPlatform.instance;
 
   test('$MethodChannelSystemBatteryInfo is the default instance', () {
     expect(initialPlatform, isInstanceOf<MethodChannelSystemBatteryInfo>());
   });
 
-  test('getPlatformVersion', () async {
+  test('getBatteryLevel', () async {
     SystemBatteryInfo systemBatteryInfoPlugin = SystemBatteryInfo();
-    MockSystemBatteryInfoPlatform fakePlatform = MockSystemBatteryInfoPlatform();
+    MockSystemBatteryInfoPlatform fakePlatform =
+        MockSystemBatteryInfoPlatform();
     SystemBatteryInfoPlatform.instance = fakePlatform;
 
-    expect(await systemBatteryInfoPlugin.getPlatformVersion(), '42');
+    expect(await systemBatteryInfoPlugin.getBatteryLevel(), 42);
   });
 }
